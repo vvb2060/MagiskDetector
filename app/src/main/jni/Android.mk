37ae1ee5a3324efbedb2c027acf9d505db685b01
vvb2060 <vvb2060@gmail.com>
2020-04-26 16:45:03 +0800

    Update NDK

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := vvb2060
-LOCAL_CFLAGS := -Wall -Wextra -Wshadow -Werror -fvisibility=hidden
+LOCAL_MODULE    := vvb2060
+LOCAL_CFLAGS    := -Oz -Wall -Wextra -Wshadow -Werror -fvisibility=hidden
 LOCAL_SRC_FILES := vvb2060.c
 LOCAL_LDLIBS    := -llog
 include $(BUILD_SHARED_LIBRARY)
