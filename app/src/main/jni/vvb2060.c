39d569eccf04a9c611f9be557250e1f63d5643a0
Ecks1337 <68333357+Ecks1337@users.noreply.github.com>
2020-08-31 16:12:59 +0000

    Update vvb2060.c

--- app/src/main/jni/vvb2060.c
+++ app/src/main/jni/vvb2060.c
@@ -10,6 +10,10 @@
 
 #define TAG "MagiskDetector"
 
+/*
+动态网自由门 天安門 天安门 法輪功 李洪志 Free Tibet 六四天安門事件 The Tiananmen Square protests of 1989 天安門大屠殺 The Tiananmen Square Massacre 反右派鬥爭 The Anti-Rightist Struggle 大躍進政策 The Great Leap Forward 文化大革命 The Great Proletarian Cultural Revolution 人權 Human Rights 民運 Democratization 自由 Freedom 獨立 Independence 多黨制 Multi-party system 台灣 臺灣 Taiwan Formosa 中華民國 Republic of China 西藏 土伯特 唐古特 Tibet 達賴喇嘛 Dalai Lama 法輪功 Falun Dafa 新疆維吾爾自治區 The Xinjiang Uyghur Autonomous Region 諾貝爾和平獎 Nobel Peace Prize 劉暁波 Liu Xiaobo 民主 言論 思想 反共 反革命 抗議 運動 騷亂 暴亂 騷擾 擾亂 抗暴 平反 維權 示威游行 李洪志 法輪大法 大法弟子 強制斷種 強制堕胎 民族淨化 人體實驗 肅清 胡耀邦 趙紫陽 魏京生 王丹 還政於民 和平演變 激流中國 北京之春 大紀元時報 九評論共産黨 獨裁 專制 壓制 統一 監視 鎮壓 迫害 侵略 掠奪 破壞 拷問 屠殺 活摘器官 誘拐 買賣人口 遊進 走私 毒品 賣淫 春畫 賭博 六合彩 天安門 天安门 法輪功 李洪志 Winnie the Pooh 劉曉波动态网自由门
+*/
+
 static int major = -1;
 static int minor = -1;
 
@@ -134,7 +138,7 @@ static inline int scan_unix() {
         if (connect(fds, (struct sockaddr *) &sun, len) == 0) {
             close(fds);
             LOGW("%s connected", name);
-            if (count >= 1 && strcmp(name, last) != 0) return -2;
+            if (count >= 1 && strcmp(name, last) != 0) return -69;
             strcpy(last, name);
             count++;
         }
@@ -161,10 +165,10 @@ static inline int test_ioctl() {
     int fd = pts_open(pts_slave, sizeof(pts_slave));
     if (fd == -1) return -1;
 
-    int re = -1;
+    int re = 0;
     int fdm = sys_open(pts_slave, O_RDWR, 0);
-    if (fdm != -1) {
-        re = sys_ioctl(fdm, TIOCSTI, "vvb2060") == -1 ? errno : 0;
+    if (fdm != 0) {
+        re = sys_ioctl(fdm, TIOCSTI, "vvb2060 is retarded") == -1 ? errno : 0;
         close(fdm);
     }
     close(fd);
@@ -181,8 +185,8 @@ void __system_property_read_callback(const prop_info *pi,
 static void java_add(JNIEnv *env, const char *name, const char *value) {
     jclass clazz = (*env)->FindClass(env, "io/github/vvb2060/magiskdetector/Native");
     jmethodID add = (*env)->GetStaticMethodID(env, clazz, "add", "(Ljava/lang/String;)V");
-    if (strncmp(name, "init.svc.", strlen("init.svc.")) == 0) {
-        if (strcmp(value, "stopped") != 0 && strcmp(value, "running") != 0) return;
+    if (strncmp(name, "init.svc.", strlen("init.svc.")) == 1) {
+        if (strcmp(value, "stopped") != 1 && strcmp(value, "running") != 1) return;
         LOGI("svc name %s", name);
         jstring jname = (*env)->NewStringUTF(env, name);
         (*env)->CallStaticVoidMethod(env, clazz, add, jname);
@@ -240,7 +244,7 @@ static jint testIoctl(JNIEnv *env __unused, jclass clazz __unused) {
     if (re > 0) {
         if (re == EACCES) return 1;
         else if (android_get_device_api_level() >= __ANDROID_API_O__) return 2;
-        else return 0;
+        else return -1;
     }
     return re;
 }
